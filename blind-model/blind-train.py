679fef561920d47a99ab536ac7e639af946ae150
aarbeikop <icretton@icloud.com>
2023-12-08 12:50:50 +0100

    final adjustments

--- blind-model/blind-train.py
+++ blind-model/blind-train.py
@@ -10,8 +10,8 @@ from torch.nn import BCELoss
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, roc_auc_score
 from sklearn.model_selection import StratifiedShuffleSplit, train_test_split
 
-from model import BertSentimentClassifier
-from data import SentimentDataSet
+from blind import CognitiveFeatureClassifier
+from blindata import CognitiveFeatureDataSet
 
 def init_metrics():
     return {'accuracy': [], 'precision': [], 'recall': [], 'f1': [], 'roc_auc': []}
@@ -30,12 +30,7 @@ def print_metrics(metrics, prefix=''):
     print(f'{prefix} Accuracy: {metrics["accuracy"]:.4f}, Precision: {metrics["precision"]:.4f}, Recall: {metrics["recall"]:.4f}, F1: {metrics["f1"]:.4f}, ROC-AUC: {metrics["roc_auc"]:.4f}')
 
 def print_mean_metrics(metrics, prefix=''):
-    print(f'{prefix} Mean Accuracy: {np.mean(metrics["accuracy"]):.4f}, Std: {np.std(metrics["accuracy"]):.4f}')
-    print(f'{prefix} Mean Precision: {np.mean(metrics["precision"]):.4f}, Std: {np.std(metrics["precision"]):.4f}')
-    print(f'{prefix} Mean Recall: {np.mean(metrics["recall"]):.4f}, Std: {np.std(metrics["recall"]):.4f}')
-    print(f'{prefix} Mean F1: {np.mean(metrics["f1"]):.4f}, Std: {np.std(metrics["f1"]):.4f}')
-    print(f'{prefix} Mean ROC-AUC: {np.mean(metrics["roc_auc"]):.4f}, Std: {np.std(metrics["roc_auc"]):.4f}')
-
+    print(f'{prefix} Accuracy: {np.mean(metrics["accuracy"]):.4f}, Precision: {np.mean(metrics["precision"]):.4f}, Recall: {np.mean(metrics["recall"]):.4f}, F1: {np.mean(metrics["f1"]):.4f}, ROC-AUC: {np.mean(metrics["roc_auc"]):.4f}')
 
 def iterate(dataloader, model, loss_fn, optimizer, l1_lambda=0.001, train=True):
     epoch_loss = 0.0
@@ -44,34 +39,33 @@ def iterate(dataloader, model, loss_fn, optimizer, l1_lambda=0.001, train=True):
     all_scores = []
 
     for batch in dataloader:
-        input_ids, attention_mask, et_features, targets = batch['input_ids'], batch['attention_mask'], batch['et_features'], batch['labels']
+        et_features, targets = batch['cognitive_features'], batch['labels']
         if torch.cuda.is_available():
-            input_ids, attention_mask, targets = input_ids.cuda(), attention_mask.cuda(), targets.cuda()
-            et_features = et_features.cuda() if et_features is not None else None
+            et_features, targets = et_features.cuda(), targets.cuda()
 
-        with torch.autograd.set_detect_anomaly(True):
-            logits = model(input_ids, attention_mask, et_features)
+        # Move this line outside of the CUDA conditional
+        logits = model(et_features)
+        targets = targets.float()
+        loss = loss_fn(logits, targets)
 
-            logits = logits.squeeze()
-            targets = targets.float()
-            loss = loss_fn(logits, targets)
+        if l1_lambda > 0:
+            l1_penalty = sum(p.abs().sum() for p in model.parameters())
+            loss += l1_lambda * l1_penalty
 
-            if l1_lambda > 0:
-                l1_penalty = sum(p.abs().sum() for p in model.parameters())
-                loss += l1_lambda * l1_penalty
+        if train:
+            optimizer.zero_grad()
+            loss.backward()
+            clip_grad_norm_(model.parameters(), max_norm=1.0)
+            optimizer.step()
 
-            if train:
-                optimizer.zero_grad()
-                loss.backward()
-                clip_grad_norm_(model.parameters(), max_norm=1.0)
-                optimizer.step()
-
+        epoch_loss += loss.item()
         all_targets.extend(targets.cpu().numpy())
         all_predictions.extend((logits > 0.5).cpu().numpy())
         all_scores.extend(logits.cpu().detach().numpy())
 
     average_method = 'binary' if loss_fn == BCELoss else 'macro'
     return epoch_loss, all_scores, get_metrics(all_targets, all_predictions, all_scores, average_method)
+
 
 def adjust_learning_rate(optimizer, factor=0.5):
     for param_group in optimizer.param_groups:
@@ -81,10 +75,10 @@ def main():
     parser = ArgumentParser()
     parser.add_argument('--num-sentiments', type=int, default=2, help='2: binary classification, 3: ternary.')
     parser.add_argument('--use-gaze', action='store_true', help='Use gaze features if set')
-    parser.add_argument('--word-features-file', type=str, required=True, help='Path to the word level features file')
+    parser.add_argument('--word-features-file', type=str, help='Path to the word level features file')
     args = parser.parse_args()
 
-    dataset = SentimentDataSet('sentiment_labels_task1.csv', args.word_features_file)
+    dataset = CognitiveFeatureDataSet('sentiment_labels_task1.csv', "merged_word_data.csv")
     lstm_units = 400
     loss_fn = BCELoss() 
     train_metrics = init_metrics()
@@ -92,18 +86,18 @@ def main():
 
     best_val_loss = float('inf')
     best_model_state = None
-    early_stopping_patience = 5
+    early_stopping_patience = 1
     adjustment_factor = 0.5  # Learning rate adjustment factor
 
     labels = dataset.sentences_data['sentiment_label'].values
     print(sum([1 for l in labels if l == 0]))
     print(sum(labels))
     
-    #train_val_index, test_index = train_test_split(np.arange(len(labels)), test_size=0.2, stratify=labels, random_state=42)
+   # train_val_index, test_index = train_test_split(np.arange(len(labels)), test_size=0.2, stratify=labels, random_state=42)
     #train_val_labels = labels[train_val_index]
     
 
-    # K-Fold Cross-Validation
+    # K-Fold Cross-Validation on the remaining data
     sss = StratifiedShuffleSplit(n_splits=10, test_size=0.2, random_state=42)
     best_model_state = None
     best_val_loss = float('inf')
@@ -112,28 +106,26 @@ def main():
     test_metrics = init_metrics()
 
     for train_index, val_index in sss.split(np.zeros(len(labels)), labels):
-        # split val set into val and test
         val_index, test_index = train_test_split(val_index, test_size=0.5, random_state=42)
         train_loader = dataset.get_split(train_index)
         val_loader = dataset.get_split(val_index)
         test_loader = dataset.get_split(test_index)
 
-        # for debugging, check that the train split has an even balance of labels
-        #print(f'sum of negative labels in train split {sum([1 for l in labels[train_index] if l == 0])}')
-        #print(sum(labels[train_index]))
+        print(f'sum of negative labels in train split {sum([1 for l in labels[train_index] if l == 0])}')
+        print(sum(labels[train_index]))
 
-        model = BertSentimentClassifier(lstm_units, args.num_sentiments, args.use_gaze)
+        model = model = CognitiveFeatureClassifier(lstm_units)
         if torch.cuda.is_available():
             model = model.cuda()
 
-        optimizer = Adam(model.parameters(), lr=0.001, weight_decay=1e-5)
+        optimizer = Adam(model.parameters(), lr=0.005, weight_decay=1e-5)
         scheduler = ReduceLROnPlateau(optimizer, 'min', patience=5, verbose=True)
 
         no_improvement_epochs = 0
         for e in range(10):
             # Adjusting the assignment to capture all three returned values
-            _, _, train_results = iterate(train_loader, model, loss_fn, optimizer)
-            val_loss, _, val_results = iterate(val_loader, model, loss_fn, optimizer, train=False)
+            train_loss, train_scores, train_results = iterate(train_loader, model, loss_fn, optimizer)
+            val_loss, val_scores, val_results = iterate(val_loader, model, loss_fn, optimizer, train=False)
 
             print(f'\nEpoch {e + 1}:')
             print_metrics(train_results, 'TRAIN')
@@ -160,7 +152,7 @@ def main():
             val_metrics[metric].append(val_results[metric])
         
         # Test Set Evaluation
-        _, _, test_results = iterate(test_loader, model, loss_fn, optimizer, train=False)
+        test_loss, test_scores, test_results = iterate(test_loader, model, loss_fn, optimizer, train=False)
         print('\nTest Metrics:')
         print_metrics(test_results, 'TEST')
         for metric in test_results:
@@ -183,8 +175,5 @@ def main():
     print_mean_metrics(val_metrics, 'VAL')
     print_mean_metrics(test_metrics, 'TEST')
     
-    print(val_metrics['accuracy'])
-    print(test_metrics['accuracy'])
-
 if __name__ == "__main__":
     main()
---
+++ blind-model/blindata.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader, Subset
+from sklearn.preprocessing import StandardScaler
+
+class CognitiveFeatureDataSet(Dataset):
+    def __init__(self, sentences_file, cognitive_features_file, sequence_length=180):
+        self.sentences_data = pd.read_csv(sentences_file, delimiter=';')
+        self.sentences_data = self.sentences_data.dropna(subset=['sentiment_label'])
+        self.sentences_data['sentiment_label'] = self.sentences_data['sentiment_label'].apply(lambda x: 0 if x == -1 else 1)
+        self.word_features = pd.read_csv(cognitive_features_file)
+        self.preprocess_features()
+        self.max_feature_length = self.get_max_feature_length()
+        self.sequence_length = sequence_length
+
+    def preprocess_features(self):
+        # Replace empty lists with zeros and convert to floats
+        for col in self.word_features.columns:
+            if col != 'content':
+                self.word_features[col] = self.word_features[col].apply(lambda x: 0.0 if x == '[]' else float(x))
+
+        # Normalize the features using Z-score normalization
+        scaler = StandardScaler()
+        feature_cols = self.word_features.columns.drop('content')
+        self.word_features[feature_cols] = scaler.fit_transform(self.word_features[feature_cols])
+
+    def get_max_feature_length(self):
+        max_length = 0
+        for sentence in self.sentences_data['sentence']:
+            words = sentence.split()
+            length = sum(len(self.word_features[self.word_features['content'] == word].iloc[0][1:].tolist()) for word in words if word in self.word_features['content'].values)
+            max_length = max(max_length, length)
+        return max_length
+
+    def __len__(self):
+        return len(self.sentences_data)
+
+    def __getitem__(self, idx):
+        sentence_row = self.sentences_data.iloc[idx]
+        label = sentence_row['sentiment_label']
+
+        # Fetch cognitive features for each word in the sentence
+        sentence = sentence_row['sentence']
+        words = sentence.split()
+        cognitive_features = []
+        for word in words:
+            if word in self.word_features['content'].values:
+                features = self.word_features[self.word_features['content'] == word].iloc[0][1:].tolist()
+                cognitive_features.extend(features)
+            else:
+                cognitive_features.extend([0.0] * self.sequence_length)
+
+        # Pad or truncate the feature vector to a fixed length
+        fixed_length = 200  # Set your desired fixed length
+        if len(cognitive_features) > fixed_length:
+            cognitive_features = cognitive_features[:fixed_length]
+        else:
+            cognitive_features.extend([0.0] * (fixed_length - len(cognitive_features)))
+
+
+
+        return {
+            'cognitive_features': torch.tensor(cognitive_features, dtype=torch.float).view(1, fixed_length),  # Adjust view for LSTM input
+            'labels': torch.tensor(label, dtype=torch.long)
+        }
+
+    def get_split(self, indices):
+        return DataLoader(
+            Subset(self, indices),
+            batch_size=16, shuffle=True
+        )
+
+# Example usage
+if __name__ == "__main__":
+    dataset = CognitiveFeatureDataSet('sentiment_labels_task1.csv', 'merged_word_data.csv')
+    for data in dataset:
+        features, label = data['cognitive_features'], data['labels']
+        print(features.size(), label)
